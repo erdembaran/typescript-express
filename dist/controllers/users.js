05d7c7b04f29fb0c8afbdd6dbd1678bfeceb6797
“Baran <109768322+erdembaran@users.noreply.github.com>
2024-04-24 18:06:35 +0300

    feat: add extend request interface

--- dist/controllers/users.js
+++ dist/controllers/users.js
@@ -2,18 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createUser = exports.getUser = exports.getUsers = void 0;
 function getUsers(req, res) {
-  res.send([]);
+    res.send([]);
 }
 exports.getUsers = getUsers;
 function getUser(req, res) {
-  res.send({});
+    res.send({});
 }
 exports.getUser = getUser;
 function createUser(req, res) {
-  return res.status(201).send({
-    id: 1,
-    username: "baran",
-    email: "baran@info.com",
-  });
+    return res.status(201).send({
+        id: 1,
+        username: "baran",
+        email: "baran@info.com",
+    });
 }
 exports.createUser = createUser;
